0508c71e385107db55e95708ce7b31dea531f6aa
Abraham Vazquez <mabraham.vazquez@gmail.com>
2025-04-15 13:44:15 -0600

    Initial Commit

---
+++ AdventurePokedexUI/AdventurePokedexUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventurePokedexUIApp.swift
+//  AdventurePokedexUI
+//
+//  Created by Abraham Vazquez on 15/04/25.
+//
+
+import SwiftUI
+
+@main
+struct AdventurePokedexUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdventurePokedexUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AdventurePokedexUI
+//
+//  Created by Abraham Vazquez on 15/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
